7be2d5815941b6a722e1ee4f16276684d2120e29
Alvisalc <alvisccy1107@gmail.com>
2023-10-10 20:15:43 -0400

    finish draft

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react' //useEffect for aos setup to ensure the animation works
 import './home.css'
 import video from '../../Assets/hero-video.mp4'
 import {GrLocation} from 'react-icons/gr'
+import {HiFilter} from 'react-icons/hi'
+import {FiFacebook} from 'react-icons/fi'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {BsListTask} from 'react-icons/bs'
+import {TbApps} from 'react-icons/tb'
+
+// AOS set up
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Home = () => {
+    // Create a react hook to add a scroll animation 
+    useEffect(()=>{
+      Aos.init({duration: 2000})
+    }, [])
+
   return (
     <section className='home'>
       <div className="overlay"></div>
@@ -12,17 +27,17 @@ const Home = () => {
       <div className="homeContent container">
         <div className="textDiv">
 
-          <span className="smallText">
+          <span data-aos="fade-up" className="smallText">
             Our Packages
           </span>
 
-          <h1 className="homeTitle">
+          <h1 data-aos="fade-up" className="homeTitle">
             Search your Holiday
           </h1>
 
         </div>
 
-        <div className="cardDiv grid">
+        <div data-aos="fade-up" className="cardDiv grid">
           <div className="destinationInput">
             <label htmlFor="city">Search your destination:</label>
             <div className="input flex">
@@ -47,8 +62,25 @@ const Home = () => {
               <input type="range" max="5000" min="1000"/>
             </div>
           </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon"/>
+            <span>MORE FILTERS</span>
+          </div>
         </div>
 
+        <div data-aos="fade-up" className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon"/>
+            <AiOutlineInstagram className="icon"/>
+            <FaTripadvisor className="icon"/>
+          </div>
+
+          <div className="leftIcons">
+            <BsListTask className="icon"/>
+            <TbApps className="icon"/>
+          </div>
+        </div>
       </div>
     </section>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
                 <div className="logoDiv">
                     <a href="/" className="logo flex">
-                        <h1><MdOutlineTravelExplore className="icon"/>Travel.</h1>
+                        <h1><MdOutlineTravelExplore className="icon"/>Traverse.</h1>
                     </a>
                 </div>
 
